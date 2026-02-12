1275cd9ad7eb64808d5d12853c74ddf00b9a821b
Captainrealbtc <otuan004@gmail.com>
2026-02-12 07:03:40 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tesla online banking',
+  webDir: 'dist'
+};
+
+export default config;
